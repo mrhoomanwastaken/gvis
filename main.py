9b1a6fae6889d754d0c5ab8ac9d8d95d96c40dcd
wyatt/mrhooman <iwrf934z2@mozmail.com>
2024-10-08 14:15:45 -0600

    move the directory for cavacore to a better place

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, GdkPixbuf , Gdk , GLib
 
 #get cavacore ready
-cava_lib = ctypes.CDLL('/home/mrhooman/Downloads/cava-0.10.2/build/libcavacore.so')
+cava_lib = ctypes.CDLL('./cavacore/build/libcavacore.so')
 
 cava_lib.cava_init.argtypes = [
     ctypes.c_int, ctypes.c_uint, ctypes.c_int, ctypes.c_int, 
