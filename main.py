0fcaf5469338baabd9e94e7e1422a8213d1f8884
mrhoomanwastaken <113311405+mrhoomanwastaken@users.noreply.github.com>
2024-10-12 20:46:52 -0600

    made the buttions work

--- main.py
+++ main.py
@@ -263,17 +263,37 @@ class MyWindow(Gtk.Window):
 
     def on_pause_button_clicked(self, button):
         if self.source:
-            self.source.PlayPause()
+            self.source.call_sync(
+                "org.mpris.MediaPlayer2.Player.PlayPause",  # D-Bus method to call
+                None,                                      # No arguments for PlayPause
+                Gio.DBusCallFlags.NONE,                    # No special flags
+                -1,                                        # No timeout
+                None                                       # No cancellable
+            )
 
     def on_back_button_clicked(self, button):
         """Skip to the previous track."""
         if self.source:
-            self.source.Previous()  # Call the Previous method from the MPRIS interface
+            self.source.call_sync(
+                "org.mpris.MediaPlayer2.Player.Previous",  # D-Bus method to call
+                None,                                      # No arguments for PlayPause
+                Gio.DBusCallFlags.NONE,                    # No special flags
+                -1,                                        # No timeout
+                None                                       # No cancellable
+            ) # Call the Previous method from the MPRIS interface
+            self.progress_bar.set_fraction(0)
+
 
     def on_skip_button_clicked(self, button):
         """Skip to the next track."""
         if self.source:
-            self.source.Next()  # Call the Next method from the MPRIS interface
+            self.source.call_sync(
+                "org.mpris.MediaPlayer2.Player.Next",  # D-Bus method to call
+                None,                                      # No arguments for PlayPause
+                Gio.DBusCallFlags.NONE,                    # No special flags
+                -1,                                        # No timeout
+                None                                       # No cancellable
+            ) # Call the Previous method from the MPRIS interface
 
     def on_draw(self, widget, cr):
         global draw_pending
